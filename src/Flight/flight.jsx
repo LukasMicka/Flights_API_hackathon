809cd3e8c31c36d1d5de60ef9e5902f6bd4ff8e4
Frantisek91 <Frantisek.Demi@gmail.com>
2019-03-15 11:22:45 +0100

    five flights

---
+++ src/Flight/flight.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { DateTime } from 'luxon';
+import './flight.scss';
+
+export default class Flight extends React.Component {
+
+    /* Duration.fromObject({this.props.dTime}).toFormat("hh:mm:ss"); */
+
+    render() {
+        return (
+          <>
+            <div className="flight">
+                <div className="cityFrom">From: {this.props.cityFrom}</div>
+                <div className="flyFrom">Airport Code: {this.props.flyFrom}</div>
+                <div className="cityTo">To: {this.props.cityTo}</div>
+                <div className="flyTo">Airport Code: {this.props.flyTo}</div>
+                <div className="price">Price: {this.props.price}</div>
+                <div className="dTime">Departure Time: {DateTime.fromMillis(this.props.dTime * 1000).toFormat('hh:mm')}</div>
+                <div className="aTime">Arrival time: {DateTime.fromMillis(this.props.aTime * 1000).toFormat('hh:mm')}</div>
+            </div>
+            <br/>
+          </>
+        );
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Quote from './Quote/quote.jsx'
+import Flight from './Flight/flight.jsx'
 import './index.scss';
 import './index.html';
 
 class App extends React.Component {
     state = {
-        quotes: [],
+        flights: [],
     }
 
     componentDidMount() {
-        fetch('http://bootcamp.podlomar.org/api/quotes')
+        fetch('https://api.skypicker.com/flights?flyFrom=PRG&to=VLC&dateFrom=16/03/2019&dateTo=17/03/2019&limit=5&partner=picky')
           .then(r => r.json())
-          .then(json => this.setState({quotes: json}));
-    }
+          .then(json => this.setState({flights: json.data}))
+          /* .then(() => console.log(this.state.flights));//TODO smazat */
 
+    }
     render() {
         return (
             <div className="container">
-                <h1>Hello World</h1>
-                { this.state.quotes.map(quote => <Quote {...quote} />) }
+                <h1>Flights</h1>
+            { this.state.flights.map((flight, index) => <Flight key={index} {...flight} />) }
             </div>
         );
     }
